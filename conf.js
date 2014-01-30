5335f3d511a5a58b7d5acea7304990a89792e431
Noah Zoschke <nzoschke@gmail.com>
2014-01-30 11:38:47 -0800

    refactor midi message config and grouping

---
+++ conf.js
@@ -0,0 +1,82 @@
+exports.midiIO = [
+  {
+    // Generic MIDI Output Device defined in djtrace.tsi
+    name: "djtrace.js",
+    port: "djtrace.js",
+    channels: {
+      183: {                                            // Ch08 in Traktor Controller Manager
+        2:    ["Deck A High Adjust",    "Deck A Mix"],  // 0-127 sliders
+        3:    ["Deck A Mid Adjust",     "Deck A Mix"],
+        4:    ["Deck A Low Adjust",     "Deck A Mix"],
+        5:    ["Deck A Volume Adjust",  "Deck A Mix"],
+        0:    ["Deck A Cue",            "Deck A Cue"],  // TODO
+
+        8:    ["Deck B High Adjust",    "Deck B Mix"],
+        9:    ["Deck B Mid Adjust",     "Deck B Mix"],
+        10:   ["Deck B Low Adjust",     "Deck B Mix"],
+        11:   ["Deck B Volume Adjust",  "Deck B Mix"],
+        0:    ["Deck B Cue",            "Deck B Cue"],
+
+        13:   ["Deck C High Adjust",    "Deck C Mix"],
+        14:   ["Deck C Mid Adjust",     "Deck C Mix"],
+        15:   ["Deck C Low Adjust",     "Deck C Mix"],
+        16:   ["Deck C Volume Adjust",  "Deck C Mix"],
+        0:    ["Deck C Cue",            "Deck C Cue"],
+
+        18:   ["Deck D High Adjust",    "Deck D Mix"],
+        19:   ["Deck D Mid Adjust",     "Deck D Mix"],
+        20:   ["Deck D Low Adjust",     "Deck D Mix"],
+        21:   ["Deck D Volume Adjust",  "Deck D Mix"],
+        0:    ["Deck D Cue",            "Deck D Cue"],
+
+        22:   ["X-Fader",               "Cross Fader"],
+
+        103:  ["Deck A Is Loaded",      "Deck A Load"],  // [0,127] toggle
+        104:  ["Deck B Is Loaded",      "Deck B Load"],
+        105:  ["Deck C Is Loaded",      "Deck C Load"],
+        106:  ["Deck D Is Loaded",      "Deck D Load"],
+
+        107:  ["Deck A Seek Position",  "Deck A Pos"],
+        108:  ["Deck B Seek Position",  "Deck B Pos"],
+        109:  ["Deck C Seek Position",  "Deck C Pos"],
+        110:  ["Deck D Seek Position",  "Deck D Pos"],
+      }
+    }
+  },
+  {
+    // Denon DN-X1600 in Layer Mode
+    // http://www.dm-pro.eu/DocumentMaster/en/DN-X1600_manual_en.pdf
+    // Page 20
+    name: "USB MIDI Device Port 1",
+    port: 0,
+    channels: {
+      176: {                                      // 0xBn in manual
+        2:    ["CH1 EQ HIGH VR",  "Deck C Mix"],  // 0-127 sliders
+        3:    ["CH1 EQ MID VR",   "Deck C Mix"],
+        4:    ["CH1 EQ LOW VR",   "Deck C Mix"],
+        5:    ["CH1 FADER",       "Deck C Mix"],
+        0:    ["CH1 CUE",         "Deck C Cue"],  // TODO: Number: 0x01; Command: SW ON : 0x9n／SW OFF : 0x8n
+
+        8:    ["CH2 EQ HIGH VR",  "Deck A Mix"],
+        9:    ["CH2 EQ MID VR",   "Deck A Mix"],
+        10:   ["CH2 EQ LOW VR",   "Deck A Mix"],
+        11:   ["CH2 FADER",       "Deck A Mix"],
+        0:    ["CH2 CUE",         "Deck A Cue"],
+
+        13:   ["CH3 EQ HIGH VR",  "Deck B Mix"],
+        14:   ["CH3 EQ MID VR",   "Deck B Mix"],
+        15:   ["CH3 EQ LOW VR",   "Deck B Mix"],
+        16:   ["CH3 FADER",       "Deck B Mix"],
+        0:    ["CH3 CUE",         "Deck B Cue"],
+
+        18:   ["CH4 EQ HIGH VR",  "Deck D Mix"],
+        19:   ["CH4 EQ MID VR",   "Deck D Mix"],
+        20:   ["CH4 EQ LOW VR",   "Deck D Mix"],
+        21:   ["CH4 FADER",       "Deck D Mix"],
+        0:    ["CH4 CUE",         "Deck D Cue"],
+
+        22:   ["CROSS FADER",     "Cross Fader"],
+      }
+    }
+  },
+]
--- midi.js
+++ midi.js
@@ -1,4 +1,5 @@
-var fs   = require("fs"),
+var conf = require("./conf"),
+    fs   = require("fs"),
     midi = require("midi"),
     mm   = require("musicmetadata"),
     path = require("path");
@@ -8,31 +9,46 @@ var cb = function(message) { console.log("MIDI DEBUG", message) }
 exports.buffer = {}
 
 exports.listen = function(opts) {
-  var portName = opts.portName  || "djtrace.js"
   if (opts.cb) cb = opts.cb
 
-  var input = new midi.input();
-  input.on("message", function(deltaTime, message) {
-    var ts      = Date.now() // track wall time of message
-    var channel = message[0]
-    var cc      = message[1]
-    var value   = message[2]
+  var channels = {}
+  var input = new midi.input()
 
-    console.log("midi message channel=" + channel + " cc=" + ("000" + cc).slice(-3) + " value=" + ("000" + value).slice(-3) + " ts=" + ts)
+  conf.midiIO.forEach(function(dev) {
+    typeof dev.port == "string" ? input.openVirtualPort(dev.port) : input.openPort(dev.port)
+    console.log("midi input open port=" + dev.port)
 
-    // Deck Is Loaded events: buffer for external processing
-    if (channel == 176) {
-      exports.buffer[ts] = message
-    }
-    // Monitor, Play/Pause or Envelope events
-    else if (channel == 177 || channel == 178 || channel == 179) {
-      message.start   = ts
-      message.content = value
-      message.group   = channel + "," + cc
-      cb(message)
+    for (channel in dev.channels) {
+      channels[channel] = dev.channels[channel]
     }
   })
 
-  console.log("midi input open name=" + portName);
-  input.openVirtualPort(portName);
+  input.on("message", function(deltaTime, message) {
+    var ts      = Date.now() // track wall time of message
+
+    var name  = null
+    var group = null
+    if (message[0] in channels && message[1] in channels[message[0]]) {
+      name  = channels[message[0]][message[1]][0]
+      group = channels[message[0]][message[1]][1]
+    }
+
+    console.log("midi message channel=" + message[0] + " cc=" + ("000" + message[1]).slice(-3) + " val=" + ("000" + message[2]).slice(-3) + " ts=" + ts + " group=" + group + " name=" + name)
+    if (!group) return
+
+    var m = {
+      start:    ts,
+      content:  message[2],
+      group:    group,
+    }
+
+    // buffer load events to later correlate with open events
+    if (group.match(/^ Load$/)) 
+      exports.buffer[ts] = m
+
+    // trigger callback for all other events
+    else 
+      cb(m)
+  })
+
 }
